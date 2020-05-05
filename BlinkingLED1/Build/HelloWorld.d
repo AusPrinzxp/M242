7ab0bc170998eac76bc728baa06f71697ff7abf0
Silvan <48201248+AusPrinzxp@users.noreply.github.com>
2020-05-05 11:17:26 +0200

    Add files via upload

---
+++ BlinkingLED1/Build/HelloWorld.d
@@ -0,0 +1,60 @@
+E:\PROJECTS\M242\avr\BlinkingLED1\Build\HelloWorld.d \
+ E:\PROJECTS\M242\avr\BlinkingLED1\Build\HelloWorld.o: \
+ E:\PROJECTS\M242\avr\BlinkingLED1\Build\HelloWorld.c \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h \
+ E:\PROJECTS\M242\avr\ToolChain\AvrLib\include/Atmega328P.h \
+ E:\PROJECTS\M242\avr\ToolChain\AvrLib\include/avrlib.h \
+ E:\PROJECTS\M242\avr\ToolChain\AvrLib\include/RegisterAccess.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\interrupt.h \
+ e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h:
+
+E:\PROJECTS\M242\avr\ToolChain\AvrLib\include/Atmega328P.h:
+
+E:\PROJECTS\M242\avr\ToolChain\AvrLib\include/avrlib.h:
+
+E:\PROJECTS\M242\avr\ToolChain\AvrLib\include/RegisterAccess.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\interrupt.h:
+
+e:\projects\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h:
